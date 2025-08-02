469a691e4e03673305f04778ccf6c16b82916a81
Abhaymani Singh <abhaymanisingh421@gmail.com>
2025-08-02 15:48:26 +0530

    added poster image, updated css

--- src/MovieCart.jsx
+++ src/MovieCart.jsx
@@ -8,7 +8,7 @@ const MovieCard = ({ movie }) => {
                     <p>{movie.Year}</p>
                 </div>
                 <div>
-                    <img src={movie.Poster !== 'N/A' ? movie.Poster : 'https://placeholder.com/400'} alt={movie.Title} />
+                    <img src={movie.Poster !== 'N/A' ? movie.Poster : '/Poster.png'} alt={movie.Title} />
                 </div>
                 <div>
                     <span>{movie.Type}</span>
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client"; 
-//88f49e76
 import App from "./App";
 const root = ReactDOM.createRoot(document.getElementById("root")); 
 root.render(<App />);
