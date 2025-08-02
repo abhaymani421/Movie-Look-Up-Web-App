65f2978812726bd21767aa08c1d08067ed9b3a77
Abhaymani Singh <abhaymanisingh421@gmail.com>
2025-08-02 15:25:24 +0530

    modified api url

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import MovieCard from "./MovieCart";
 import './App.css'
 import searchIcon from './search.svg'
 
-const API_URL = `http://www.omdbapi.com?apikey=${process.env.REACT_APP_OMDB_API_KEY}`;
+const API_URL = `https://www.omdbapi.com?apikey=${process.env.REACT_APP_OMDB_API_KEY}`;
 
 
 const App = ()=>{
